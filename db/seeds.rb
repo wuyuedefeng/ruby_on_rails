c5445f1c77524347720a71e6dd13bada9d66efc0
sen <578595193@qq.com>
2017-04-02 22:33:42 +0800

    导出模式，将表结构导出到另一个数据库！

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,10 @@
 # 不同环境中运行迁移
 # rake db:migrate RAILS_ENV=test
 
+# 导出模式
+# 在 config/database.yml中修改数据库名称
+# 执行 rake db:schema:load     ## 加载schema中的结构到新配置的数据库
+
 10.times do |n|
   User.create(:name => "name#{n}", :phone_number => n, :gender => '男')
 end
