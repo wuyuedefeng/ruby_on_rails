aa59324faf8cc3798236b3b77199fc27658e267b
sen <578595193@qq.com>
2017-04-03 10:30:31 +0800

    Model 验证： 存在  不存在  长度

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,11 @@
 class User < ActiveRecord::Base
-   validates :name, presence: true
-   validates :phone_number, presence: true
+   validates :name, presence: true # 不能为空
+   # validates :name, length: {minimum: 3} # 最少3个字符
+   # validates :name, length: {maximum: 20} # 最多20个字符
+   # validates :name, length: {in: 3..20} # 3 - 20 个字符
+   # validates :name, length: {is: 5} # 必须是5个字符
+
+
+   validates :phone_number, presence: true # 不能为空
+   # validates :phone_number, absence: true # 必须为空
 end
