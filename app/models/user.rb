960eb740f06682403eda42504527bb9a8f37590b
sen <578595193@qq.com>
2017-04-03 10:55:17 +0800

    密码存在性验证：只在创建的时候验证

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,8 +12,11 @@ class User < ActiveRecord::Base
    # validates :phone_number, format: {with: /1[0-9]{10}\z/} # 正则匹配验证
 
   # validates :password, confirmation: true # 自动添加虚拟字段： password_confirmation, password_confirmation不为空才会验证
-  # validates :password_confirmation, presence: true
+  # validates :password, :password_confirmation, presence: {on: :create} # 只在创建的时候去验证存在性
 
   # validates :province, inclusion: {in: ["北京", "上海"]} # province的值必须在列表中
   # validates :province, exclusion: {in: ["日本"]} # province的值不能是列表中的值
+
+  # 条件验证
+
 end
