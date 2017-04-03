b99366b70aa2ee408bd8241c9e2b1a666c21e621
sen <578595193@qq.com>
2017-04-03 10:34:25 +0800

    密码验证， 数据一致性判断

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,4 +8,7 @@ class User < ActiveRecord::Base
 
    validates :phone_number, presence: true # 不能为空
    # validates :phone_number, absence: true # 必须为空
+
+  validates :password, confirmation: true # 自动添加虚拟字段： password_confirmation, password_confirmation不为空才会验证
+  validates :password_confirmation, presence: true
 end
