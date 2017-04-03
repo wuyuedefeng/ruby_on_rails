ba221a922f39ed0e8f2c1a1ce04ec833d7bb2226
sen <578595193@qq.com>
2017-04-03 10:44:15 +0800

    .

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,18 @@
 class User < ActiveRecord::Base
-   validates :name, presence: true # 不能为空
+   # validates :name, presence: true # 不能为空
    # validates :name, length: {minimum: 3} # 最少3个字符
    # validates :name, length: {maximum: 20} # 最多20个字符
    # validates :name, length: {in: 3..20} # 3 - 20 个字符
    # validates :name, length: {is: 5} # 必须是5个字符
-   validates :name, uniqueness: true
+   # validates :name, uniqueness: true
 
 
-   validates :phone_number, presence: true # 不能为空
+   # validates :phone_number, presence: true # 不能为空
    # validates :phone_number, absence: true # 必须为空
-   validates :phone_number, format: {with: /1[0-9]{10}\z/} # 正则匹配验证
+   # validates :phone_number, format: {with: /1[0-9]{10}\z/} # 正则匹配验证
 
-  validates :password, confirmation: true # 自动添加虚拟字段： password_confirmation, password_confirmation不为空才会验证
-  validates :password_confirmation, presence: true
+  # validates :password, confirmation: true # 自动添加虚拟字段： password_confirmation, password_confirmation不为空才会验证
+  # validates :password_confirmation, presence: true
 
   # validates :province, inclusion: {in: ["北京", "上海"]} # province的值必须在列表中
   # validates :province, exclusion: {in: ["日本"]} # province的值不能是列表中的值
