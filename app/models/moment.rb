419c4ec74475c43a9d7f6657abc0337fd824381b
sen <578595193@qq.com>
2017-04-05 10:41:41 +0800

    .

--- app/models/moment.rb
+++ app/models/moment.rb
@@ -1,5 +1,8 @@
 # 朋友圈
 class Moment < ActiveRecord::Base
   belongs_to :user
+  # rails 4.1之前
+  # belongs_to :user, :inverse_of => :moments
+
   has_one  :location, :as => :loc
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,14 @@
 # 用户
 class User < ActiveRecord::Base
   has_many :moments
+  # rails 4.1之前
+  # has_many :moments, :inverse_of => :user
+
   has_and_belongs_to_many :official_accounts, :join_table => "user_follow_official_accounts"
+
   # 多态
   has_one  :location, :as => :loc
+
   # 自关联
   has_and_belongs_to_many :followers, :join_table => "user_follow_users", :class_name => "User", :foreign_key => "followee_id", :association_foreign_key => "follower_id"
 end
